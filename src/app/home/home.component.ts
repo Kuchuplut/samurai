d34ce8ebbd62fc8ea79cfdc86c24a176ff73a501
killandibuz <killandibuz@gmail.com>
2020-09-24 23:09:50 +0800

    Add Truffle connection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransferService } from '../shared/services/transfer.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  public user: any = {};
+  constructor(private transferService: TransferService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user.address = '';
+    this.user.balance = null;
+
+    this.getAccountAndBalance();
+  }
+
+  getAccountAndBalance = () => {
+    const that = this;
+    this.transferService
+      .getUserBalance()
+      .then(function (retAccount: any) {
+        that.user.address = retAccount.account;
+        that.user.balance = retAccount.balance;
+        console.log('transfer.components :: getAccountAndBalance :: that.user');
+        console.log(that.user);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  transferEther() {
+    this.transferService.transferEther({
+      transferAddress: '',
+      account: '',
+      amount: '',
+    });
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { TransferService } from '../shared/services/transfer.service';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [SharedModule, CommonModule, HomeRoutingModule],
+  providers: [TransferService],
 })
 export class HomeModule {}
